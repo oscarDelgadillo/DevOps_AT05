aeb640217daafa4abed08ec57908b629a91bfc2a
oscarDelgadillo <oscar.delgadillo@fundacion-jala.org>
2018-02-16 15:39:37 -0400

    Adding vagrant setup file and environment variables

---
+++ environments.bat
@@ -0,0 +1,12 @@
+@echo "start enviroments"
+@echo off
+set OS_AUTH_URL=http://10.28.3.240:5000/v3
+set OS_TENANT_NAME=e07d76b7e23f4eef8e0fb13f7b19e6f9
+set OS_USERNAME=oscardelgadillo
+set OS_PASSWORD=Pajaro123
+set OS_DOMAIN_NAME=fundacion-jala
+set OS_PROJECT_NAME=fj_oscardelgadillo_at05-desktop
+set OS_FLAVOR=f1.desktop.medium
+set OS_IMAGE=ubuntu-16.04-amd64-server_12012018
+set OS_KEY_PAIR_NAME=VirtualKey
+set OS_REGION_NAME=bootstack-jalasoft
