9329c197a080b54051bb2979a0441365507f9732
oscarDelgadillo <oscar.delgadillo@fundacion-jala.org>
2018-02-22 13:39:11 -0400

    task3: adding webApplication files and adding fakes unit test

---
+++ ubuntu16_server/webApplication/quickstart/src/test/java/org/gradle/sample/GreeterTest.java
@@ -0,0 +1,91 @@
+package org.gradle.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Administrator on 2/22/2018.
+ */
+public class GreeterTest {
+
+  /**
+   * First Test.
+   */
+  @Test
+  public void firstTest() {
+    assertTrue(true);
+  }
+
+  /**
+   * Second Test.
+   */
+  @Test
+  public void secondTest() {
+    assertTrue(true);
+  }
+
+  /**
+   * Third Test.
+   */
+  @Test
+  public void thirdTest() {
+    assertTrue(true);
+  }
+
+  /**
+   * Fourth Test.
+   */
+  @Test
+  public void fourthTest() {
+    assertTrue(true);
+  }
+
+  /**
+   * Fifth Test.
+   */
+  @Test
+  public void fifthTest() {
+    assertTrue(true);
+  }
+
+  /**
+   * Sixth Test.
+   */
+  @Test
+  public void sixthTest() {
+    assertTrue(true);
+  }
+
+  /**
+   * Seventh Test.
+   */
+  @Test
+  public void seventhTest() {
+    assertTrue(true);
+  }
+
+  /**
+   * Eighth Test.
+   */
+  @Test
+  public void eighthTest() {
+    assertTrue(true);
+  }
+
+  /**
+   * Ninth Test.
+   */
+  @Test
+  public void ninthTest() {
+    assertTrue(true);
+  }
+
+  /**
+   * Tenth Test.
+   */
+  @Test
+  public void tenthTest() {
+    assertTrue(true);
+  }
+}
